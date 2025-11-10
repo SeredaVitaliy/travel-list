cf00fc55585a4e607e4813a55547fa2115830e29
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-10 20:31:23 +0200

    Добавление формы

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 export function App() {
   return (
     <div className="app">
@@ -15,18 +21,52 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 function Form() {
+  function handeSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handeSubmit}>
       <h3>Whath do you need for your trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..."></input>
+      <button>ADD</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox"></input>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>💼 You have X items on your list, and you already packed X (X%)</em>
     </footer>
   );
