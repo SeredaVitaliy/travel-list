0ba3764cdf435e297a73afbde7d4b7af5ed94cd4
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-12 08:52:35 +0200

    Добавлен функционал сортировки

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -79,10 +79,25 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -91,6 +106,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Cортировка по добавлению</option>
+          <option value="description">Сортировка по описанию</option>
+          <option value="packed">По статусу упакованно</option>
+        </select>
+      </div>
     </div>
   );
 }
